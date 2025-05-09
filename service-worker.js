1a89b970e35976b76a17b071f11cfe3ed17b247d
dedicatedcloud <98156514+dedicatedcloud@users.noreply.github.com>
2025-05-09 14:29:23 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,22 @@
-self.addEventListener("install", e => {
+const CACHE_NAME = "triage-cache-v1";
+const BASE_PATH = "/gp-triage-pwa/";
+
+self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open("triage-cache-v1").then(cache =>
+    caches.open(CACHE_NAME).then((cache) =>
       cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./icons/icon-192.png",
-        "./icons/icon-512.png"
+        BASE_PATH,
+        BASE_PATH + "index.html",
+        BASE_PATH + "manifest.json",
+        BASE_PATH + "icons/icon-192.png",
+        BASE_PATH + "icons/icon-512.png"
       ])
     )
   );
 });
-self.addEventListener("fetch", e => {
+
+self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
